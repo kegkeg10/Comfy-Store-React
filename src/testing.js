a0bb80784bd7844db17b8b76b355d4d766f631f4
Nick Garner <nickgcs13@gmail.com>
2023-04-04 20:19:00 -0500

    Navbar

---
+++ src/testing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const testing = () => {
+  return (
+    <Wrapper>
+        <h3>Hello wo</h3>
+    </Wrapper>
+  )
+}
+
+export default testing
+
+const Wrapper = styled.section `
+  
+`
