946212ca1aee57bde40e82ec4ade2ca82db5f08e
Nick Garner <nickgcs13@gmail.com>
2023-04-08 21:53:13 -0500

    day 4

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,7 +1,7 @@
-import axios from "axios";
-import React, { useContext, useEffect, useReducer } from "react";
-import reducer from "../reducers/products_reducer";
-import { products_url as url } from "../utils/constants";
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import reducer from '../reducers/products_reducer';
+import { products_url as url } from '../utils/constants';
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -10,8 +10,8 @@ import {
   GET_PRODUCTS_ERROR,
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR,
-} from "../actions";
+  GET_SINGLE_PRODUCT_ERROR
+} from '../actions';
 
 const initialState = {
   isSidebarOpen: false,
@@ -21,7 +21,7 @@ const initialState = {
   featured_products: [],
   single_product_loading: false,
   single_product_error: false,
-  single_product: {},
+  single_product: {}
 };
 
 const ProductsContext = React.createContext();
@@ -32,11 +32,12 @@ export const ProductsProvider = ({ children }) => {
   const openSidebar = () => {
     dispatch({ type: SIDEBAR_OPEN });
   };
+
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
-  const fetchProducts = async (url) => {
+  const fetchProducts = async url => {
     dispatch({ type: GET_PRODUCTS_BEGIN });
     try {
       const response = await axios.get(url);
@@ -47,7 +48,7 @@ export const ProductsProvider = ({ children }) => {
     }
   };
 
-  const fetchSingleProducts = async (url) => {
+  const fetchSingleProduct = async url => {
     dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
     try {
       const response = await axios.get(url);
@@ -63,12 +64,14 @@ export const ProductsProvider = ({ children }) => {
   }, []);
 
   return (
-    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
+    >
       {children}
     </ProductsContext.Provider>
   );
 };
-// make sure use
+// make sure to use
 export const useProductsContext = () => {
   return useContext(ProductsContext);
 };
